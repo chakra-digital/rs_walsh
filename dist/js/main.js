dc35473ca3b3d764ed182180a3f6f61a0a4c53ec
Matt McDonald <mcdonamj087@gmail.com>
2019-11-09 01:17:43 -0600

    Started and nearly finished projects page

--- dist/js/main.js
+++ dist/js/main.js
@@ -175,3 +175,79 @@ if(dropdownWrappers !== undefined) {
 }
 
 
+
+
+
+
+
+/********************************
+ Projects Page
+********************************/
+
+
+const projects = document.querySelectorAll('.project-wrap');
+
+const maxTransform = 15;
+const duration = 0.6;
+const easeFunction = 'Power3.easeOut'
+
+
+projects.forEach( project => project.addEventListener('mouseleave', (function() {
+  const projectText = project.querySelectorAll('h2');
+  const image = project.querySelector('img');
+  
+  TweenMax.to([projectText, image], duration, {
+    y: 0, 
+		x: 0,
+		scale: 1,
+    ease: easeFunction
+  }); 
+
+})))
+
+projects.forEach( project => project.addEventListener('mousemove', (function(e){
+  const bcr = project.getBoundingClientRect();
+  const xPos = bcr.x;
+  const yPos = bcr.y;
+  const w = bcr.width;
+  const h = bcr.height;
+  
+  // Get mouse position relative to window
+  const mouseWindowY = e.clientY;
+  const mouseWindowX = e.clientX;
+  
+  // Get mouse position relative to project - center point being zero
+  const mouseProjectX = Math.floor(mouseWindowX - xPos - w/2);
+  const mouseProjectY = Math.floor(mouseWindowY - yPos - h/2);
+  
+  // Get mouse position on project card
+  const mousePercentageX = Math.round(1000 * (mouseProjectX / (w/2))) / 1000;
+  const mousePercentageY = Math.round(1000 * (mouseProjectY / (h/2))) / 1000;
+  
+  // console.log(mousePercentageX, mousePercentageY)
+  
+  const projectText = project.querySelectorAll('h2');
+  const image = project.querySelector('img');
+  
+  
+  
+  projectText.forEach(text => {
+    TweenMax.to(text, duration, {
+      y: maxTransform * -mousePercentageY, 
+			x: maxTransform * -mousePercentageX,
+      ease: easeFunction,
+      // transformPerspective: 900,
+      // transformOrigin: 'center'
+    });  
+  })
+  
+  TweenMax.to(image, duration, {
+    y: maxTransform * mousePercentageY, 
+    x: maxTransform * mousePercentageX,
+		scale: 1.1,
+    ease: easeFunction
+  });
+  
+})));
+
+
